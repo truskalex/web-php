bdefe01d410628626ffc7ad09dd55c15b7e05acd
Daniil <1xs@pm.me>
2020-02-25 07:00:21 +0000

    work

---
+++ app/libs/Controller.php
@@ -0,0 +1,14 @@
+<?php
+//class controller | loads views and models
+
+class Controller
+{
+//load view
+    public function view($viewFile, $data = array()){
+        if(file_exists('../app/views'.$viewFile.'.php')){
+            require_once '../app/views'.$viewFile.'.php';
+        }else{
+            die($viewFile.'.php does not exist');
+        }
+    }
+}
