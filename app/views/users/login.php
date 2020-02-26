9d4dd6100763e0814649ebfa78c54865245a8f78
Daniil <1xs@pm.me>
2020-02-26 07:01:40 +0000

    yes

--- app/views/users/login.php
+++ app/views/users/login.php
@@ -1,4 +1,27 @@
 <?php require_once APPROOT.'/views/inc/header.php';?>
-login
+<div class="row">
+    <div class="col-md-6 mx-auto">
+        <div class="card card/body bg-light mt-5">
+            <h2>Log-In</h2>
+            <form action="<?php echo URLROOT;?>/users/register" method="post">
+                <div class="form-group">
+                    <label for="name">Email: <sup>*</sup></label>
+                    <input type="email" id="email" class="form-control">
+                </div>
+
+                <div class="form-group">
+                    <label for="name">Password: <sup>*</sup></label>
+                    <input type="password" id="pass" class="form-control">
+                </div>
+
+                <div class="row">
+                    <div class="col">
+                        <input type="submit" value="Login" class="btn btn-block bg-info">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
 <?php require_once APPROOT.'/views/inc/footer.php';?>
 
--- app/views/users/register.php
+++ app/views/users/register.php
@@ -1,4 +1,37 @@
 <?php require_once APPROOT.'/views/inc/header.php';?>
-register
+<div class="row">
+    <div class="col-md-6 mx-auto">
+        <div class="card card/body bg-light mt-5">
+            <h2>Register</h2>
+            <form action="<?php echo URLROOT;?>/users/register" method="post">
+                <div class="form-group">
+                    <label for="name">Name: <sup>*</sup></label>
+                        <input type="text" id="name" class="form-control">
+                </div>
+
+                <div class="form-group">
+                    <label for="name">Email: <sup>*</sup></label>
+                    <input type="email" id="email" class="form-control">
+                </div>
+
+                <div class="form-group">
+                    <label for="name">Password: <sup>*</sup></label>
+                    <input type="password" id="pass" class="form-control">
+                </div>
+
+                <div class="form-group">
+                    <label for="name">Confirm password: <sup>*</sup></label>
+                    <input type="password" id="pass2" class="form-control">
+                </div>
+
+                <div class="row">
+                    <div class="col">
+                        <input type="submit" value="Register" class="btn btn-block bg-info">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
 <?php require_once APPROOT.'/views/inc/footer.php';?>
 
