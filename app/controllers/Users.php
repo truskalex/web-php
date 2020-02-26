1f5051d91aa5680e6debe122c4a8ba3bb42a8156
Daniil <1xs@pm.me>
2020-02-26 07:44:36 +0000

    Register form

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -8,6 +8,39 @@ class Users extends Controller
   }
 
   public function register(){
-    $this->view('users/register');
+      if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+          echo 'data is sent';
+          $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+          $data = array(
+            'name' => trim($_POST['name']),
+            'email' => trim($_POST['email']),
+            'pass' => trim($_POST['pass']),
+            'pass2' => trim($_POST['pass2']),
+            'name_err' => '',
+              'email_err' => '',
+              'pass_err' => '',
+              'pass2_err' => '',
+          );
+
+          if (empty($data['name'])) {
+              $data['name_err'] = 'Please enter the name';
+          }
+              if (empty($data['email'])) {
+                  $data['email_err'] = 'Please enter the email';
+              }
+              if (empty($data['pass'])) {
+                  $data['pass_err'] = 'Please enter the pass';
+              }
+              if (empty($data['pass2'])){
+              $data['pass2_err'] = 'Please enter the pass2';
+          }
+          echo '<pre>';
+          print_r($data);
+          echo '</pre>';
+          $this->view('users/register', $data);
+      } else{
+          echo 'yeetr';
+          $this->view('users/register');
+      }
   }
 }
--- app/views/users/register.php
+++ app/views/users/register.php
@@ -6,22 +6,26 @@
             <form action="<?php echo URLROOT;?>/users/register" method="post">
                 <div class="form-group">
                     <label for="name">Name: <sup>*</sup></label>
-                        <input type="text" id="name" class="form-control">
+                        <input type="text" id="name" class="form-control <?php echo (!empty($data['name_err'])) ? 'is-invalid' : '';?>" name="name">
+                    <span class="invalid-feedback"><?php echo $data['name_err'];?></span>
                 </div>
 
                 <div class="form-group">
                     <label for="name">Email: <sup>*</sup></label>
-                    <input type="email" id="email" class="form-control">
+                    <input type="email" id="email" class="form-control <?php echo (!empty($data['email_err'])) ? 'is-invalid' : '';?>" name="email">
+                    <span class="invalid-feedback"><?php echo $data['email_err'];?></span>
                 </div>
 
                 <div class="form-group">
                     <label for="name">Password: <sup>*</sup></label>
-                    <input type="password" id="pass" class="form-control">
+                    <input type="password" id="pass" class="form-control <?php echo (!empty($data['pass_err'])) ? 'is-invalid' : '';?>" name="pass">
+                    <span class="invalid-feedback"><?php echo $data['pass_err'];?></span>
                 </div>
 
                 <div class="form-group">
                     <label for="name">Confirm password: <sup>*</sup></label>
-                    <input type="password" id="pass2" class="form-control">
+                    <input type="password" id="pass2" class="form-control <?php echo (!empty($data['pass2_err'])) ? 'is-invalid' : '';?>" name="pass2">
+                    <span class="invalid-feedback"><?php echo $data['pass2_err'];?></span>
                 </div>
 
                 <div class="row">
