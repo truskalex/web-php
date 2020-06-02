bbed72e736c858d4601279558b3b0823a73bc0f8
Alex Trusk <37287790+truskalex@users.noreply.github.com>
2020-06-02 12:01:46 +0300

    Update Post.php

--- app/models/Post.php
+++ app/models/Post.php
@@ -3,5 +3,69 @@
 
 class Post
 {
+    private $db;
+
+    public function __construct(){
+        $this->db = new Database();
+    }
+
+    public function getAllPosts(){
+        $this->db->query('SELECT *,
+                        posts.id as postID,
+                        users.id as userID,
+                        users.created_at as userCreated
+                        FROM posts 
+                        INNER JOIN users
+                        ON posts.user_id = users.id 
+                        ORDER BY posts.created_at DESC');
+        $posts = $this->db->getAll();
+        if($this->db->rowCount() > 0){
+            return $posts;
+        } else {
+            return false;
+        }
+    }
+
+    public function getPostById($id){
+        $this->db->query('SELECT * FROM posts WHERE id = :id');
+        $this->db->bind(':id', $id);
+        $post = $this->db->getOne();
+        return $post;
+    }
+
+    public function addPost($data){
+        $this->db->query('INSERT INTO posts (title, user_id, content) VALUES (:title, :user_id, :content)');
+        $this->db->bind(':title', $data['title']);
+        $this->db->bind(':user_id', $data['user_id']);
+        $this->db->bind(':content', $data['content']);
+        if($this->db->execute()){
+            // query is submitted
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public function editPost($data){
+        $this->db->query('UPDATE posts SET title=:title, content=:content WHERE id=:id');
+        $this->db->bind(':id', $data['id']);
+        $this->db->bind(':title', $data['title']);
+        $this->db->bind(':content', $data['content']);
+        $result = $this->db->execute();
+        if($result){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public function deletePost($id){
+        $this->db->query('DELETE FROM posts WHERE id=:id');
+        $this->db->bind(':id', $id);
+        $result = $this->db->execute();
+        if($result){
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 }
