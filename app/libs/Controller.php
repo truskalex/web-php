b80f0e38acaf33618da6abb1b1bc40029705ee47
Alex Trusk <37287790+truskalex@users.noreply.github.com>
2020-06-02 12:00:30 +0300

    Update Controller.php

--- app/libs/Controller.php
+++ app/libs/Controller.php
@@ -7,19 +7,19 @@
  */
 class Controller
 {
-  // load view
-  public function view($viewFile, $data = array()){
-    if(file_exists('../app/views/'.$viewFile.'.php')){
-      require_once '../app/views/'.$viewFile.'.php';
-    } else {
-      die($viewFile.'.php does not exist');
+    // load view
+    public function view($viewFile, $data = array()){
+        if(file_exists('../app/views/'.$viewFile.'.php')){
+            require_once '../app/views/'.$viewFile.'.php';
+        } else {
+            die($viewFile.'.php does not exist');
+        }
     }
-  }
 
-  // load model
-  public function model($modelFile){
-    require_once '../app/models/'.$modelFile.'.php';
-    $model = new $modelFile();
-    return $model;
-  }
+    // load model
+    public function model($modelFile){
+        require_once '../app/models/'.$modelFile.'.php';
+        $model = new $modelFile();
+        return $model;
+    }
 }
