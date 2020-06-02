c443a963fb944ed5cff62856555e47bc8b543a98
Alex Trusk <37287790+truskalex@users.noreply.github.com>
2020-06-02 12:00:54 +0300

    Update Core.php

--- app/libs/Core.php
+++ app/libs/Core.php
@@ -7,42 +7,42 @@
 
 class Core
 {
-  protected $currentController = 'Posts';
-  protected $currentMethod = 'index';
-  protected $params = array();
+    protected $currentController = 'Posts';
+    protected $currentMethod = 'index';
+    protected $params = array();
 
-  public function __construct()
-  {
-    $url = $this->getUrl();
-    // set up the controller
-    if(file_exists('../app/controllers/'.ucwords($url[0]).'.php')){
-      $this->currentController = ucwords($url[0]);
-      unset($url[0]);
-    }
-    // create controller object
-    require_once '../app/controllers/'.$this->currentController.'.php';
-    $this->currentController = new $this->currentController();
-    // check the method exist and set up it
-    if(isset($url[1])){
-      if(method_exists($this->currentController, $url[1])){
-        $this->currentMethod = $url[1];
-        unset($url[1]);
-      }
-    }
-    // set up params
-    $this->params = $url ? array_values($url) : array();
+    public function __construct()
+    {
+        $url = $this->getUrl();
+        // set up the controller
+        if(file_exists('../app/controllers/'.ucwords($url[0]).'.php')){
+            $this->currentController = ucwords($url[0]);
+            unset($url[0]);
+        }
+        // create controller object
+        require_once '../app/controllers/'.$this->currentController.'.php';
+        $this->currentController = new $this->currentController();
+        // check the method exist and set up it
+        if(isset($url[1])){
+            if(method_exists($this->currentController, $url[1])){
+                $this->currentMethod = $url[1];
+                unset($url[1]);
+            }
+        }
+// set up params
+        $this->params = $url ? array_values($url) : array();
 
-    // call a callback function with array of params
-    call_user_func_array(array($this->currentController, $this->currentMethod), $this->params);
-  }
-
-  // get url from link and prepare for use
-  public function getUrl(){
-    if(isset($_GET['url'])){
-      $url = rtrim($_GET['url'], '/');
-      $url = filter_var($url,FILTER_SANITIZE_URL);
-      $url = explode('/', $url);
-      return $url;
+// call a callback function with array of params
+        call_user_func_array(array($this->currentController, $this->currentMethod), $this->params);
     }
-  }
+
+    // get url from link and prepare for use
+    public function getUrl(){
+        if(isset($_GET['url'])){
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url,FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
+        }
+    }
 }
