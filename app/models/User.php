ceef979e32ef25a58972be387fe0698165e70d53
Alex Trusk <37287790+truskalex@users.noreply.github.com>
2020-06-02 12:02:01 +0300

    Update User.php

--- app/models/User.php
+++ app/models/User.php
@@ -3,44 +3,51 @@
 
 class User
 {
-  private $db;
+    private $db;
 
-  public function __construct(){
-    $this->db = new Database();
-  }
-  // check email
-  public function findUserByEmail($email){
-    $this->db->query('SELECT * FROM users WHERE email=:email');
-    $this->db->bind(':email', $email);
-    $this->db->execute();
-    if($this->db->rowCount() > 0){
-      return true;
-    } else {
-      return false;
+    public function __construct(){
+        $this->db = new Database();
     }
-  }
-  // register user
-  public function register($data){
-    $this->db->query('INSERT INTO users (name, email, pass) VALUES (:name, :email, :pass )');
-    $this->db->bind(':name', $data['name']);
-    $this->db->bind(':email', $data['email']);
-    $this->db->bind(':pass', $data['pass']);
-    if($this->db->execute()){
-      return true;
-    } else {
-      return false;
+    // check email
+    public function findUserByEmail($email){
+        $this->db->query('SELECT * FROM users WHERE email=:email');
+        $this->db->bind(':email', $email);
+        $this->db->execute();
+        if($this->db->rowCount() > 0){
+            return true;
+        } else {
+            return false;
+        }
     }
-  }
-  // login user
-  public function login($email, $pass){
-    $this->db->query('SELECT * FROM users WHERE email=:email');
-    $this->db->bind(':email', $email);
-    $user = $this->db->getOne();
-    $userHashedPass = $user->pass;
-    if(password_verify($pass, $userHashedPass)){
-      return $user;
-    } else {
-      return false;
+    // register user
+    public function register($data){
+        $this->db->query('INSERT INTO users (name, email, pass) VALUES (:name, :email, :pass )');
+        $this->db->bind(':name', $data['name']);
+        $this->db->bind(':email', $data['email']);
+        $this->db->bind(':pass', $data['pass']);
+        if($this->db->execute()){
+            return true;
+        } else {
+            return false;
+        }
     }
-  }
+    // login user
+    public function login($email, $pass){
+        $this->db->query('SELECT * FROM users WHERE email=:email');
+        $this->db->bind(':email', $email);
+        $user = $this->db->getOne();
+        $userHashedPass = $user->pass;
+        if(password_verify($pass, $userHashedPass)){
+            return $user;
+        } else {
+            return false;
+        }
+    }
+    // get user by id
+    public function getUserById($id){
+        $this->db->query('SELECT * FROM users WHERE id=:id');
+        $this->db->bind(':id', $id);
+        $user = $this->db->getOne();
+        return $user;
+    }
 }
